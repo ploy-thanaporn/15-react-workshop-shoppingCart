b6ff5d5ca6a54b63b4787f50e79fc751ee07add8
Thanaporn Sae-pueng <78015751+ploy-thanaporn@users.noreply.github.com>
2022-06-23 16:18:44 +0700

    first commit

---
+++ src/data/CartData.js
@@ -0,0 +1,33 @@
+const CartData = [
+  {
+    id: 1,
+    name: "Lauv",
+    image_url: "https://pbs.twimg.com/media/Dd_9rTdV0AA8pLm.jpg",
+    price: 5600,
+    quantity: 1,
+  },
+  {
+    id: 2,
+    name: "Keshi",
+    image_url: "https://biggo.co.th/s/cd+keshi/",
+    price: 2600,
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: "Taylor Swift (RED)",
+    image_url: "https://i.ebayimg.com/images/g/-dkAAOSwN5phoDB1/s-l1600.jpg",
+    price: 2800,
+    quantity: 1,
+  },
+  {
+    id: 4,
+    name: "The 1975: A Brief Inquiry Into Online Relationships (CD)",
+    image_url:
+      "https://happeningandfriends.com/uploads/happening/products/23/002227/the1975.jpg",
+    price: 1500,
+    quantity: 1,
+  },
+];
+
+export default CartData;
